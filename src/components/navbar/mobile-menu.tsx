3ce82c38c6628113295df0c472fc7ae2da780e0c
Mohamedreda03 <mo03.reda@gmail.com>
2024-04-30 18:34:34 +0300

    Edit the menu to close when you click on the link.

--- src/components/navbar/mobile-menu.tsx
+++ src/components/navbar/mobile-menu.tsx
@@ -2,6 +2,7 @@
 
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -10,18 +11,50 @@ import {
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 
-import MobileNavLinks from "./mobile-nav-links";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+const navData = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "Menu",
+    link: "/menu",
+  },
+];
 
 export default function MobileMenu() {
+  const pathname = usePathname();
   return (
     <div className="md:hidden flex items-center">
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Menu className="h-10 w-10" />
         </SheetTrigger>
         <SheetContent>
           <div className="mt-20">
-            <MobileNavLinks />
+            <ul className="flex items-center gap-6 font-medium flex-col">
+              {navData.map((item) => {
+                const isActive = item.link === pathname;
+                return (
+                  <li key={item.id} className="flex w-full ml-6 text-lg">
+                    <SheetClose asChild>
+                      <Link
+                        href={item.link}
+                        className={cn(isActive && "text-orange-500", "w-full")}
+                      >
+                        {item.label}
+                      </Link>
+                    </SheetClose>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </SheetContent>
       </Sheet>
